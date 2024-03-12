146fed57a07182f6cfc34a96dc1a543a40073a1c
tobiasazpia <tobiasazpia@gmail.com>
2024-03-11 23:11:32 -0300

    Users (#1)
    * Firts Commit
    
    * Usuarios

---
+++ UserManager.js
@@ -0,0 +1,26 @@
+class UserManager {
+    static #users = [];
+    create(data){
+        const user = {
+            //id: UserManager.#users.length+1,
+            id: UserManager.#users.length === 0
+                ? 1
+                : UserManager.#users[UserManager.#users.length-1].id+1,
+            foto: data.foto,
+            mail: data.mail,
+            password: data.password,
+            role: 0
+        };
+        UserManager.#users.push(user);
+    }
+
+    read(){
+        return UserManager.#users
+    }
+} 
+
+const usuarios = new UserManager();
+
+usuarios.create({foto: "foto.png", mail: "email@ejemplo.com" , password: "1234"});
+
+console.log(usuarios.read())
